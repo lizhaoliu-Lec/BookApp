736064487aa8e14a8e6b70aff1e231b105d79d0e
lizhaoliu_Bak <13611425862llz@gmail.com>
2019-08-13 23:39:46 +0800

    add timing app.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'apps.user',
-    'apps.bookstore',
+    'apps.timing',
 ]
 
 MIDDLEWARE = [
@@ -104,6 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+
 
 TIME_ZONE = 'UTC'
 
